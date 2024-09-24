0b1d0487b1520d622d4746624302960ad1252b3f
Agustina Insua <aguss.insua99@gmail.com>
2024-09-24 12:51:31 -0300

    update dependencies

--- exchange-front/src/app/app.routes.ts
+++ exchange-front/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo:'dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path:'dashboard',
+        component:DashboardComponent,
+        title:'Dashboard'
+    },
+    {
+        path:'login',
+        component:LoginComponent,
+        title:'Login'
+    }
+];
