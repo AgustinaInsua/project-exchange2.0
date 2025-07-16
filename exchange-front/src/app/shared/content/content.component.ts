ad2e659e4feeb8addc9abbb1a5d8d64e8383cbf4
Agustina Insua <aguss.insua99@gmail.com>
2025-07-16 11:51:38 -0300

    Update content.component.ts

--- exchange-front/src/app/shared/content/content.component.ts
+++ exchange-front/src/app/shared/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
